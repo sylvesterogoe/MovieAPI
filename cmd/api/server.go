90e30c863f633ac77ebe89a379f672a140464438
Sylvester Ogoe <Sylvesterogoe@gmail.com>
2023-04-24 21:08:11 +0000

    Corrected environment name

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -25,7 +25,7 @@ func main() {
 
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "environment", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
