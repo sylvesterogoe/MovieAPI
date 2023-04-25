2ea55eff3bd0264423969307549ff809bac7bcae
Sylvester Ogoe <Sylvesterogoe@gmail.com>
2023-04-25 19:00:48 +0000

    Extracted routes and used httprouter package

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -10,3 +10,11 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 	fmt.Fprintf(w, "environment: %s\n", app.config.env)
 	fmt.Fprintf(w, "version: %s\n", version)
 }
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "movie showed")
+}
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "movie created")
+}
--- cmd/api/server.go
+++ cmd/api/server.go
@@ -35,12 +35,9 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
-
 	srv := http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      mux,
+		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
@@ -48,7 +45,7 @@ func main() {
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	err := srv.ListenAndServe()
-	if err != nil{
+	if err != nil {
 		logger.Fatal(err)
 	}
 
