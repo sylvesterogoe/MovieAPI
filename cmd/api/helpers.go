57c00562164ffb06cc30b88ee984abd788788c69
Sylvester Ogoe <Sylvesterogoe@gmail.com>
2023-04-26 22:51:40 +0000

    Added json response functionality for structs

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -11,8 +12,29 @@ import (
 func (app *application) readIDParam(r *http.Request) (int64, error) {
 	param := httprouter.ParamsFromContext(r.Context()).ByName("id")
 	id, err := strconv.ParseInt(param, 10, 64)
-	if id <= 0 || err != nil{
+	if id <= 0 || err != nil {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	jsn, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Newline to make viewing easier in terminal applications.
+	jsn = append(jsn, '\n')
+
+	// It's OK if the provided header map is nil. Go doesn't throw an error
+	// if you try to range over (or generally, read from) a nil map.
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(jsn)
+	return nil
 }
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"starlet.sylvester.net/internal/data"
 )
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,7 +14,21 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "showing details of movie %d\n", id)
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Akacalito",
+		Year:      2023,
+		Runtime:   120,
+		Genres:    []string{"drama", "comedy", "adventure"},
+		Version:   1,
+	}
+
+	app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
