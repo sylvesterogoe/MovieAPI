9295eb66f8eecc92902cc6870c1bf39602035f48
Sylvester Ogoe <Sylvesterogoe@gmail.com>
2023-05-04 18:30:41 +0000

    Fixed typo

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -11,7 +11,7 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version":     version,
 	}
 
-	err := app.writeJSON(w, http.StatusOK, envelope{"healtcheck": data}, nil)
+	err := app.writeJSON(w, http.StatusOK, envelope{"healthcheck": data}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
