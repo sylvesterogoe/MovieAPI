1eed3987e3590e8fa1d0f9790fa2cd092ca96c07
Sylvester Ogoe <Sylvesterogoe@gmail.com>
2023-05-07 12:05:11 +0000

    Added movie update logic

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/lib/pq"
@@ -22,26 +23,72 @@ type MovieModel struct {
 	DB *sql.DB
 }
 
-func (m MovieModel) Insert(movie *Movie) error{
+func (m MovieModel) Insert(movie *Movie) error {
 	query :=
-	 	`INSERT INTO movies (title, year, runtime, genres)
+		`INSERT INTO movies (title, year, runtime, genres)
 		VALUES ($1, $2, $3, $4)
 		RETURNING id, created_at, version`
-	
+
 	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
 
 	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
-func (m MovieModel) Get(id int64) (*Movie, error){
-	return nil, nil
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	if id < 1 {
+		return nil, ErrRecordNotFound
+	}
+
+	query :=
+		`SELECT id, created_at, title, year, runtime, genres, version
+		FROM movies
+		WHERE id = $1`
+
+	var movie Movie
+
+	args := []interface{}{
+		&movie.ID, 
+		&movie.CreatedAt, 
+		&movie.Title, 
+		&movie.Year,
+		&movie.Runtime, 
+		pq.Array(&movie.Genres), 
+		&movie.Version}
+
+	err := m.DB.QueryRow(query, id).Scan(args...)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &movie, nil
+
 }
 
-func (m MovieModel) Update(movie *Movie) error{
-	return nil
+func (m MovieModel) Update(movie *Movie) error {
+	query := 
+		`UPDATE movies
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5
+		RETURNING version`
+
+	args := []interface{}{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
-func (m MovieModel) Delete(id int64) error{
+func (m MovieModel) Delete(id int64) error {
 	return nil
 }
 
