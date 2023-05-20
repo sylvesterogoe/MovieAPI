2056344e8f2e5be2d63f260a143ad955ebf1b4ff
Sylvester Ogoe <Sylvesterogoe@gmail.com>
2023-05-20 15:08:10 +0000

    Added query string validations

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -9,29 +9,28 @@ import (
 	"starlet.sylvester.net/internal/validator"
 )
 
-
 func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title    string
-		Genres   []string
-		Page     int
-		PageSize int
-		Sort     string
+		Title  string
+		Genres []string
+		data.Filters
 	}
-	
+
 	v := validator.New()
 	qs := r.URL.Query()
-	
+
 	input.Title = app.readString(qs, "title", "")
 	input.Genres = app.readCSV(qs, "genres", []string{})
-	input.Page = app.readInt(qs, "page", 1, v)
-	input.PageSize = app.readInt(qs, "page_size", 20, v)
-	input.Sort = app.readString(qs, "sort", "id")
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
+	input.Filters.Sort = app.readString(qs, "sort", "id")
+	input.Filters.SortSafeList = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
 
-	if !v.Valid() {
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
+	
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
