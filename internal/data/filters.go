b00b0beda70cd318d74af0e1d22c2e4870f275c3
sylvesterogoe <sylvesterogoe@gmail.com>
2023-05-27 15:07:56 +0000

    Added metadata for for easier pagination requests

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,10 +1,19 @@
 package data
 
 import (
+	"math"
 	"strings"
 
 	"starlet.sylvester.net/internal/validator"
 )
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page,omitempty"`
+	PageSize     int `json:"page_size,omitempty"`
+	FirstPage    int `json:"first_page,omitempty"`
+	LastPage     int `json:"last_page,omitempty"`
+	TotalRecords int `json:"total_records,omitempty"`
+}
 
 type Filters struct {
 	Page         int
@@ -35,6 +44,20 @@ func (f Filters) limit() int {
 
 func (f Filters) offset() int {
 	return (f.Page - 1) * f.PageSize
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		// Note that we return an empty Metadata struct if there are no records.
+		return Metadata{}
+	}
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
